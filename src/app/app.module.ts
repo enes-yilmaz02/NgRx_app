39886b2d5374e34c112693be2435014889ae324d
enes-yilmaz02 <ey684241@gmail.com>
2022-11-02 16:22:00 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule } from '@angular/router';
 import {StoreModule} from '@ngrx/store';
 import { AppComponent } from './app.component';
 
@@ -10,6 +10,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    RouterModule,
     StoreModule.forRoot({
       
     })
